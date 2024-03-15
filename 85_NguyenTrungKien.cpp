9a587e87a8c16e5370c51da273fd5116d3bad24d
MTBMM <kiennguyentrung408@gmail.com>
2024-03-15 15:53:46 +0700

    demo update

--- 85_NguyenTrungKien.cpp
+++ 85_NguyenTrungKien.cpp
@@ -1,9 +1,4 @@
-﻿print("helloffffffffff")
-print("Nhanh Ho Quoc Trung")
-<<<<<<< HEAD
-print(" tao Nhanh Ho Quoc Trung")
-print(" tao Nhanh Ho Quoc Trung")
-=======
-print("Tạo lệnh từ nhanh main")
-print("Tạo lệnh từ nhanh main")
->>>>>>> b41d5fbba082b39980486fa429c37615dccb9f66
+﻿print(" tao Nhanh từ demo")
+
+
+
